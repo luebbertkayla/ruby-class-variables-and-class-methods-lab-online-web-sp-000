0d7805aba8d47356638dd4ff8218e1664c67a297
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-16 18:14:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,5 +36,17 @@ class Song
       end 
     end
     genre_count
-  end   
+  end  
+  
+  def self.artist.count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1 
+      end
+    end
+    artist_count
+  end
 end
