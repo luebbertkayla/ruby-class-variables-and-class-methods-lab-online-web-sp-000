d19534959490922f8c3aadf9604f147efbe2fbb4
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-16 17:40:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,11 @@ class Song
   
   @@song_count = 0
   
-  def initialize
+  def initialize(name, artist, genre)
     @@song_count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
   end
   
   def self.count 
